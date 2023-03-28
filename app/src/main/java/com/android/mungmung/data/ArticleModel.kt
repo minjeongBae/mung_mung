2a1e3791323d41beecda426a3a829be70f1de984
HyunJin Ko <iamkokoko123@gmail.com>
2023-03-28 21:24:20 +0900

    animation, push firestore

---
+++ app/src/main/java/com/android/mungmung/data/ArticleModel.kt
@@ -0,0 +1,8 @@
+package com.android.mungmung.data
+
+data class ArticleModel(
+    val articleId: String? = null,
+    val createdAt: Long? = null,
+    val description: String? = null,
+    val imageUrl: String? = null
+)
--- app/src/main/java/com/android/mungmung/ui/home/AddArticleFragment.kt
+++ app/src/main/java/com/android/mungmung/ui/home/AddArticleFragment.kt
@@ -10,10 +10,12 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.android.mungmung.R
+import com.android.mungmung.data.ArticleModel
 import com.android.mungmung.databinding.FragmentAddArticleBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import java.util.*
@@ -29,9 +31,8 @@ class AddArticleFragment: Fragment(R.layout.fragment_add_article) {
         if (uri != null) {
             selectedUri = uri
             binding.photoImageView.setImageURI(selectedUri)
-            Log.d("PhotoPicker", "Selected URI: $uri")
-        } else {
-            Log.d("PhotoPicker", "No media selected")
+            binding.clearImageView.isVisible = false
+            binding.addImageView.isVisible = true
         }
     }
 
@@ -39,54 +40,24 @@ class AddArticleFragment: Fragment(R.layout.fragment_add_article) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentAddArticleBinding.bind(view)
 
-
         startPicker()
         setupPhotoImageView()
         setupClearImageView()
+        setupBackImageButton()
+        setupSubmitButton(view)
 
-        binding.addButton.setOnClickListener {
+    }
 
-            if (selectedUri != null) {
-                val photoUri = selectedUri ?: return@setOnClickListener
-                val fileName = "${UUID.randomUUID()}.png"
+    private fun startPicker() {
+        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
 
-                Firebase.storage.reference.child("articles").child(fileName)
-                    .putFile(photoUri)
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            Log.d("upload image", "success")
-                            Firebase.storage.reference.child("articles").child(fileName)
-                                .downloadUrl
-                                .addOnSuccessListener {
-                                    Log.d("upload image", it.toString())
-
-                                    // todo animation, .... +@
-                                    findNavController().navigate(AddArticleFragmentDirections.actionBack())
-
-                                }.addOnFailureListener {
-
-                                }
-
-                        } else {
-
-                            task.exception?.printStackTrace()
-                        }
-                    }
-
-            } else {
-                Snackbar.make(view, "이미지가 선택되지 않았습니다", Snackbar.LENGTH_SHORT).show()
+    private fun setupPhotoImageView() {
+        binding.addImageView.setOnClickListener {
+            if (selectedUri == null) {
+                startPicker()
             }
         }
-
-        binding.backImageButton.setOnClickListener {
-            findNavController().navigate(AddArticleFragmentDirections.actionBack())
-        }
-
-
-
-
-
-
     }
 
     private fun setupClearImageView() {
@@ -99,19 +70,104 @@ class AddArticleFragment: Fragment(R.layout.fragment_add_article) {
         }
     }
 
+    private fun setupBackImageButton() {
+        binding.backImageButton.setOnClickListener {
+            findNavController().navigate(AddArticleFragmentDirections.actionBack())
+        }
+    }
 
-    private fun setupPhotoImageView() {
-        binding.addImageView.setOnClickListener {
-            if (selectedUri == null) {
-                startPicker()
+    private fun setupSubmitButton(view: View) {
+        binding.addButton.setOnClickListener {
+            showProgress()
+
+            if (selectedUri != null) {
+                val photoUri = selectedUri ?: return@setOnClickListener
+                uploadImage(
+                    uri = photoUri,
+                    successHandler = {
+                        // Fire store 에 데이터 업로드
+                        uploadArticle(it, binding.descriptionEditText.text.toString())
+                    },
+                    errorHandler = {
+                        Snackbar.make(view, "이미지 업로드에 실패했습니다.", Snackbar.LENGTH_SHORT).show()
+                        Log.e("ERROR", it.toString())
+                        hideProgress()
+                    }
+                )
+
+            } else {
+                Snackbar.make(view, "이미지가 선택되지 않았습니다", Snackbar.LENGTH_SHORT).show()
+                hideProgress()
             }
         }
     }
 
-    private fun startPicker() {
-        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+
+
+    private fun uploadImage(
+        uri: Uri,
+        successHandler: (String) -> Unit,
+        errorHandler: (Throwable?) -> Unit
+    ) {
+        val fileName = "${UUID.randomUUID()}.png"
+        Firebase.storage.reference.child("articles").child(fileName)
+            .putFile(uri)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+
+                    Firebase.storage.reference.child("articles").child(fileName)
+                        .downloadUrl
+                        .addOnSuccessListener {
+                            successHandler(it.toString())
+
+                        }.addOnFailureListener {
+                            errorHandler(it)
+                        }
+
+                } else {
+                    errorHandler(task.exception)
+                }
+            }
     }
 
+
+    private fun showProgress() {
+        binding.progressBarLayout.isVisible = true
+    }
+
+    private fun hideProgress() {
+        binding.progressBarLayout.isVisible = false
+    }
+
+    private fun uploadArticle(photoUrl: String, description: String) {
+        val articleId = UUID.randomUUID().toString()
+        val articleModel = ArticleModel(
+            articleId = articleId,
+            createdAt = System.currentTimeMillis(),
+            description = description,
+            imageUrl = photoUrl
+        )
+
+        Firebase.firestore.collection("articles").document(articleId)
+            .set(articleModel)
+            .addOnSuccessListener {
+                findNavController().navigate(AddArticleFragmentDirections.actionBack())
+
+                view?.let { view ->
+                    Snackbar.make(view, "글 작성에 성공했습니다.", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+            .addOnFailureListener {
+                it.printStackTrace()
+                view?.let { view ->
+                    Snackbar.make(view, "글 작성에 실패했습니다.", Snackbar.LENGTH_SHORT).show()
+                }
+                hideProgress()
+            }
+
+        hideProgress()
+
+    }
 
 
 }

c58569ee76e896c7210d873d8d2e0943af586434
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2023-04-04 02:54:24 +0900

    setup views in profile_fragment

--- app/src/main/java/com/android/mungmung/ui/ProfileFragment.kt
+++ app/src/main/java/com/android/mungmung/ui/ProfileFragment.kt
@@ -2,21 +2,29 @@ package com.android.mungmung.ui
 
 import android.os.Bundle
 import android.view.*
-import androidx.core.view.MenuHost
-import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
-import androidx.navigation.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.android.mungmung.R
 import com.android.mungmung.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
     private lateinit var binding: FragmentProfileBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding = FragmentProfileBinding.bind(view)
+        auth = Firebase.auth
+
+        binding.profileRecyclerview.apply {
+            layoutManager = GridLayoutManager(context, 3)
+        }
+
     }
 
 

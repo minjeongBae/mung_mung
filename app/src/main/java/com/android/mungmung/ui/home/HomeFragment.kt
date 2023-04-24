c1f274f5d4cd9d8637e73b21b55f0ad880a2217b
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2023-04-24 16:16:38 +0900

    Remove article

--- app/src/main/java/com/android/mungmung/ui/home/HomeFragment.kt
+++ app/src/main/java/com/android/mungmung/ui/home/HomeFragment.kt
@@ -1,7 +1,6 @@
 package com.android.mungmung.ui.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
